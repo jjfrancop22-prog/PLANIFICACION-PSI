d2992fba0069d379113483678b614f1583da92b5
jjfrancop22-prog <jjfrancop22@hotmail.com>
2026-09-24 07:52:41 -0500

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const SW_VERSION='V1.0.5.6.33.25.10.17';
+const SW_VERSION='V1.0.5.6.33.25.10.18';
 const CACHE_NAME='erp-planificacion-v1.0.5.6.33.25.10.17';
 const APP_SHELL=[
   './','./index.html','./styles.css','./app.js','./firebase-config.js',
